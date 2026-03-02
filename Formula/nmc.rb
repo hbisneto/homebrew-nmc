dfbdac15a609bd92528e922e4ddf8a8b4b1ee062
Heitor Bardemaker A. Bisneto <hbarbisneto@gmail.com>
2026-03-02 16:04:05 -0300

    Add additional resources for requests, certifi, charset-normalizer, idna, and urllib3 in nmc formula

--- Formula/nmc.rb
+++ Formula/nmc.rb
@@ -14,6 +14,31 @@ class Nmc < Formula
     sha256 "9ffc4cf0c6c5192280b8cd8cd1504a85fbb5c0fafd36faa7cfc9207e98b8da49"
   end
 
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/source/r/requests/requests-2.32.5.tar.gz"
+    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/source/c/certifi/certifi-2026.2.25.tar.gz"
+    sha256 "e887ab5cee78ea814d3472169153c2d12cd43b14bd03329a39a9c6e2e80bfba7"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/source/c/charset-normalizer/charset_normalizer-3.4.4.tar.gz"
+    sha256 "94537985111c35f28720e43603b8e7b43a6ecfb2ce1d3058bbe955b73404e21a"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/source/i/idna/idna-3.11.tar.gz"
+    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/source/u/urllib3/urllib3-2.6.3.tar.gz"
+    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
+  end
+
   def install
     virtualenv_install_with_resources
   end
